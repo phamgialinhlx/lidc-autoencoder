a76f53e50a850dd397cd2d84b861ea1d9dba44ec
phamgialinhlx <phamgialinhlx2@gmail.com>
2024-03-30 10:36:41 +0900

    fix: classification metrics and loss weight config

--- src/callbacks/classification_metrics.py
+++ src/callbacks/classification_metrics.py
@@ -4,7 +4,7 @@ from torchmetrics import MaxMetric, MeanMetric
 from lightning.pytorch.callbacks import Callback
 
 class ClassificationMetrics(Callback):
-    def __init__(self, num_classes: int = 2):
+    def __init__(self, num_classes: int = 2, device="cpu"):
         super().__init__()
         self.num_classes = num_classes
         self.train_acc = Accuracy(task="multiclass", num_classes=num_classes, average="micro")
@@ -32,39 +32,42 @@ class ClassificationMetrics(Callback):
 
         self.val_acc_best = float("-inf")
         
+        self.device = device
+
     def on_fit_start(self, trainer, pl_module):
-        self.train_acc = self.train_acc.to(pl_module.device)
-        self.val_acc = self.val_acc.to(pl_module.device)
-        self.test_acc = self.test_acc.to(pl_module.device)
+        self.train_acc = self.train_acc.to(self.device)
+        self.val_acc = self.val_acc.to(self.device)
+        self.test_acc = self.test_acc.to(self.device)
 
-        self.train_f1 = self.train_f1.to(pl_module.device)
-        self.train_precision = self.train_precision.to(pl_module.device)
-        self.train_recall = self.train_recall.to(pl_module.device)
-        self.train_kappa = self.train_kappa.to(pl_module.device)
+        # self.train_f1 = self.train_f1.to(pl_module.device)
+        self.train_precision = self.train_precision.to(self.device)
+        self.train_recall = self.train_recall.to(self.device)
+        self.train_kappa = self.train_kappa.to(self.device)
 
-        self.val_f1 = self.val_f1.to(pl_module.device)
-        self.val_precision = self.val_precision.to(pl_module.device)
-        self.val_recall = self.val_recall.to(pl_module.device)
-        self.val_kappa = self.val_kappa.to(pl_module.device)
+        self.val_f1 = self.val_f1.to(self.device)
+        self.val_precision = self.val_precision.to(self.device)
+        self.val_recall = self.val_recall.to(self.device)
+        self.val_kappa = self.val_kappa.to(self.device)
 
-        self.test_f1 = self.test_f1.to(pl_module.device)
-        self.test_precision = self.test_precision.to(pl_module.device)
-        self.test_recall = self.test_recall.to(pl_module.device)
-        self.test_kappa = self.test_kappa.to(pl_module.device)
+        self.test_f1 = self.test_f1.to(self.device)
+        self.test_precision = self.test_precision.to(self.device)
+        self.test_recall = self.test_recall.to(self.device)
+        self.test_kappa = self.test_kappa.to(self.device)
         
-        self.train_loss = self.train_loss.to(pl_module.device)
-        self.val_loss = self.val_loss.to(pl_module.device)
-        self.test_loss = self.test_loss.to(pl_module.device)
+        self.train_loss = self.train_loss.to(self.device)
+        self.val_loss = self.val_loss.to(self.device)
+        self.test_loss = self.test_loss.to(self.device)
 
-    def reset(self):
-    # def on_train_epoch_start(self, trainer, pl_module):
+    # def reset(self):
+    def on_train_epoch_start(self, trainer, pl_module):
         self.train_acc.reset()
         self.train_f1.reset()
         self.train_precision.reset()
         self.train_recall.reset()
         self.train_kappa.reset()
         self.train_loss.reset()
-    # def on_validation_epoch_start(self, trainer, pl_module):
+    
+    def on_validation_epoch_start(self, trainer, pl_module):
         self.val_acc.reset()
         self.val_f1.reset()
         self.val_precision.reset()
@@ -73,7 +76,7 @@ class ClassificationMetrics(Callback):
         self.val_loss.reset()
 
 
-    # def on_test_epoch_start(self, trainer, pl_module):
+    def on_test_epoch_start(self, trainer, pl_module):
         self.test_acc.reset()
         self.test_f1.reset()
         self.test_precision.reset()
@@ -86,36 +89,36 @@ class ClassificationMetrics(Callback):
         preds = outputs["cls_preds"]
         targets = batch["label"]
 
-        self.train_loss(loss)
-        self.train_acc(preds, targets)
-        self.train_f1(preds, targets)
-        self.train_precision(preds, targets)
-        self.train_recall(preds, targets)
-        self.train_kappa(preds, targets)
+        self.train_loss(loss.to(self.device))
+        self.train_acc(preds.to(self.device), targets.to(self.device))
+        self.train_f1(preds.to(self.device), targets.to(self.device))
+        self.train_precision(preds.to(self.device), targets.to(self.device))
+        self.train_recall(preds.to(self.device), targets.to(self.device))
+        self.train_kappa(preds.to(self.device), targets.to(self.device))
 
     def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx=0):
         loss = outputs["cls_loss"]
         preds = outputs["cls_preds"]
         targets = batch["label"]
 
-        self.val_loss(loss)
-        self.val_acc(preds, targets)
-        self.val_f1(preds, targets)
-        self.val_precision(preds, targets)
-        self.val_recall(preds, targets)
-        self.val_kappa(preds, targets)
+        self.val_loss(loss.to(self.device))
+        self.val_acc(preds.to(self.device), targets.to(self.device))
+        self.val_f1(preds.to(self.device), targets.to(self.device))
+        self.val_precision(preds.to(self.device), targets.to(self.device))
+        self.val_recall(preds.to(self.device), targets.to(self.device))
+        self.val_kappa(preds.to(self.device), targets.to(self.device))
 
     def on_test_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx=0):
         loss = outputs["cls_loss"]
         preds = outputs["cls_preds"]
         targets = batch["label"]
 
-        self.test_loss(loss)
-        self.test_acc(preds, targets)
-        self.test_f1(preds, targets)
-        self.test_precision(preds, targets)
-        self.test_recall(preds, targets)
-        self.test_kappa(preds, targets)
+        self.test_loss(loss.to(self.device))
+        self.test_acc(preds.to(self.device), targets.to(self.device))
+        self.test_f1(preds.to(self.device), targets.to(self.device))
+        self.test_precision(preds.to(self.device), targets.to(self.device))
+        self.test_recall(preds.to(self.device), targets.to(self.device))
+        self.test_kappa(preds.to(self.device), targets.to(self.device))
 
     def on_validation_epoch_end(self, trainer, pl_module):
         if not trainer.sanity_checking:
@@ -162,6 +165,4 @@ class ClassificationMetrics(Callback):
                 pl_module.log("test/precision", self.test_precision, metric_attribute="test_precision", on_step=False, on_epoch=True, prog_bar=True)
                 pl_module.log("test/recall", self.test_recall, metric_attribute="test_recall", on_step=False, on_epoch=True, prog_bar=True)
                 pl_module.log("test/cohen_kappa", self.test_kappa, metric_attribute="test_kappa", on_step=False, on_epoch=True, prog_bar=True)
-            
-            self.reset()
             
